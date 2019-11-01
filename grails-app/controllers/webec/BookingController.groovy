bed2c30bb35009437aee7a1100188ad11c8ec930
Steve Vogel <steve.vogel@students.fhnw.ch>
2019-11-01 13:47:18 +0100

    after week 7, first part

--- grails-app/controllers/webec/BookingController.groovy
+++ grails-app/controllers/webec/BookingController.groovy
@@ -2,8 +2,13 @@ package webec
 
 class BookingController {
 
-    void probiere(){
-        
+    def probiere(){
+        // find the last names of all people that booked
+        // room named "1.313"
+        def room = Room.findByName("1.313")
+        def result = Booking.findAllByRoom(room)*.booker.lastName.unique()
+        render text: result.toString()
+
     }
 
     static scaffold = Booking
